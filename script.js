1adb6316375c1bf1293cadd3ab4cd6d100a22270
harika234 <harikacdm@gmail.com>
2024-04-03 10:17:09 +0530

    updated

---
+++ script.js
@@ -0,0 +1,18 @@
+function scrollTo(elementId) {
+    const element = document.getElementById(elementId);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToMenu() {
+    scrollTo('menu');
+}
+function scrollToMenu() {
+    scrollTo('menu');
+}
+function scrollToMenu() {
+    scrollTo('menu');
+}
+function scrollToMenu() {
+    scrollTo('menu');
+}
+
